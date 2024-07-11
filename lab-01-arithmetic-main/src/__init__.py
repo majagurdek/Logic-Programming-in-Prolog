d312dbed7ea5a2aa592adf24ef280279d5900c13
majagurdek <72552429+majagurdek@users.noreply.github.com>
2024-07-11 19:57:54 +0200

    Delete lab-01-arithmetic-main directory

--- lab-01-arithmetic-main/src/__init__.py
+++
@@ -1,4 +0,0 @@
-#  Copyright (c) 2021. Created by Mateusz Slazynski for the educational purposes.
-#     Feel free to use/modify this code for any greater good.
-#     It would be nice however if you mentioned me somewhere.
-#     Still, no pressure - have a nice day!
